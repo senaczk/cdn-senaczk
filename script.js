0e95c9002922ae5c3c3f860d12feff2eb0ee80ff
senaczk <arsena.dev@gmail.com>
2026-05-25 09:46:31 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const developer = "Sena"
+
+console.log(`Hello from ${developer}`)
+
+const skills = [
+"HTML",
+"CSS",
+"JavaScript",
+"NodeJS",
+"REST API"
+]
+
+skills.forEach(skill=>{
+console.log(skill)
+})
